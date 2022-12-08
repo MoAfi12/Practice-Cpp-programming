bc427de31531a6f2e2d80a60ced4475468be785a
Mohamed Afi <maxamedcaafi60@gmail.com>
2022-12-09 01:36:20 +0300

    Practice day2

---
+++ day2/Project1/Project1/Function.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int addition(int a, int b);
+
+
+	int main() {
+		int  mohamed = addition(10 ,30);
+		std::cout << mohamed << std::endl;
+		
+	}
+
+	int addition(int a, int b)
+	{
+
+		int zahid = a + b; 
+		return zahid;
+
+	}
+
+
+	//is the about function
+
+
+
